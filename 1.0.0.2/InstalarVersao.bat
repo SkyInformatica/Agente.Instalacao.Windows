e0f61090bca6d163da150501b9be0dceeff1ad83
Bruno Deuner <bruno.deuner@skyinformatica.com.br>
2021-07-21 19:00:46 -0300

    Commit parcial, em fase de testes

--- 1.0.0.2/InstalarVersao.bat
+++ 1.0.0.2/InstalarVersao.bat
@@ -1,25 +1,16 @@
 @ECHO off
 @CHCP 65001
 
-set caminhoDoAppsettings=%1
-
-if (caminhoDoAppsettings neq '') (
-	echo "Não foi informado o caminho do appsettings como parametro"
-	exit /b 1;
-)
-
-set versao=1.0.0.2
-set nomeServico=SkyAgenteNovoProjeto%versao%
+set versaoAnterior=1.0.0.2
+set versao=1.0.0.6
+set nomeServico=SkyInfo.Agente.Servico.Agente
 set nomeDoExecutavel=SkyInfo.Agente.Servico.Agente.exe
 
-set caminhoDeExecucaoAtual=%cd%
-pushd ..
-set caminhoAnterior=%cd%
-popd
+cd ..
 
-copy /Y %caminhoDoAppsettings% %caminhoAnterior%/%nomeServico%/appsettings.json
+copy /Y %versaoAnterior%\binarios\appsettings.json %versao%\binarios\appsettings.json
 
-sc create %nomeServico% binPath=%caminhoDeExecucaoAtual%/%versao%/Binarios/%nomeDoExecutavel% start=auto
+sc create %nomeServico% binPath=%cd%\%versao%\Binarios\%nomeDoExecutavel% start=auto
 sc description %nomeServico% "Agente de sincronização dos sistemas legado com novo projeto Sky Sistemas."
 sc failure %nomeServico% reset=86400 actions=restart/180000/restart/180000/restart/180000
 sc start %nomeServico%
