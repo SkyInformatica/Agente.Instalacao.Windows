c49875d5fe373498aa4fedf390e564fd0fcb3787
brunodeunersky <bruno.deuner@skyinformatica.com.br>
2021-07-23 11:07:42 -0300

    Atualizado script de atualizar para ultima versão

--- AtualizarParaUltimaVersao.bat
+++ AtualizarParaUltimaVersao.bat
@@ -2,7 +2,9 @@
 
 set versaoAInstalar=1.0.2.3
 
-sc create SkyInfo.Servico.Agente.%versaoAInstalar% binPath="%cd%\%versaoAInstalar%\SkyInfo.Agente.Servico.Agente.exe" start=delayed-auto
-sc description SkyInfo.Servico.Agente.%versaoAInstalar% "Agente de sincronização dos sistemas legado com novo projeto Sky Sistemas."
+cd ../..
+
+sc create SkyInfo.Servico.Agente.%versaoAInstalar% binPath="%cd%\%versaoAInstalar%\Binarios\SkyInfo.Agente.Servico.Agente.exe" start=delayed-auto
+sc description SkyInfo.Servico.Agente.%versaoAInstalar% "Agente de sincronizaÃ§Ã£o dos sistemas legado com novo projeto Sky Sistemas."
 sc failure SkyInfo.Servico.Agente.%versaoAInstalar% reset=86400 actions=restart/180000/restart/180000/restart/180000
 sc start SkyInfo.Servico.Agente.%versaoAInstalar%
