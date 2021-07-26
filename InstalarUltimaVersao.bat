90d895eb0b5908b6479bc15f47aaa3e9ecd12771
Alex Junior Vieira <alex.vieira@skyinformatica.com.br>
2021-07-26 14:26:10 -0300

    scripts bat alterados para CRLF

--- InstalarUltimaVersao.bat
+++ InstalarUltimaVersao.bat
@@ -1,12 +1,21 @@
 @CHCP 65001
 
-set versaoAInstalar=1.0.0.2
 set appSettingsEmTexto=%1
-set pastaTemporarioParaSalvarOAppsettings=%temp%/sky/agente/
-set caminhoTemporarioParaSalvarOAppsettings=%pastaTemporarioParaSalvarOAppsettings%/appSettings.json
+
+echo %1 > appSettings.json
 
 appSettingsEmTexto > %caminhoTemporarioParaSalvarOAppsettings%
 
 %versaoAInstalar%/InstalarVersao %versaoAInstalar% %caminhoTemporarioParaSalvarOAppsettings%
 
 del %caminhoTemporarioParaSalvarOAppsettings%
+
+
+set versaoAInstalar=1.0.2.3
+
+cd ../..
+
+sc create SkyInfo.Servico.Agente.%versaoAInstalar% binPath="%cd%\%versaoAInstalar%\Binarios\SkyInfo.Agente.Servico.Agente.exe" start=delayed-auto
+sc description SkyInfo.Servico.Agente.%versaoAInstalar% "Agente de sincronização dos sistemas legado com novo projeto Sky Sistemas."
+sc failure SkyInfo.Servico.Agente.%versaoAInstalar% reset=86400 actions=restart/180000/restart/180000/restart/180000
+sc start SkyInfo.Servico.Agente.%versaoAInstalar%
