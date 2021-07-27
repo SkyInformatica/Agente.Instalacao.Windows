b25c04c125286a3b555f6fa277571f7e42e05e04
Alex Junior Vieira <alex.vieira@skyinformatica.com.br>
2021-07-27 16:35:08 -0300

    copiando appsettings para repositorio da versão

--- InstalarUltimaVersao.bat
+++ InstalarUltimaVersao.bat
@@ -2,6 +2,8 @@
 
 set versaoAInstalar=1.0.2.3
 
+copy %cd%\appsettings.json %cd%\repositorio\%versaoAInstalar%\Binarios\appsettings.json
+
 sc create SkyInfo.Servico.Agente.%versaoAInstalar% binPath="%cd%\repositorio\%versaoAInstalar%\Binarios\SkyInfo.Agente.Servico.Agente.exe" start=delayed-auto
 sc description SkyInfo.Servico.Agente.%versaoAInstalar% "Agente de sincronização dos sistemas legado com novo projeto Sky Sistemas."
 sc failure SkyInfo.Servico.Agente.%versaoAInstalar% reset=86400 actions=restart/180000/restart/180000/restart/180000
