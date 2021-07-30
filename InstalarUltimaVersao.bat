c1125875fff13a9ac788337163a3c76f6073f2f5
Alex Junior Vieira <alex.vieira@skyinformatica.com.br>
2021-07-30 09:46:35 -0300

    adicionado parametro para instalar e desinstalar

--- InstalarUltimaVersao.bat
+++ InstalarUltimaVersao.bat
@@ -1,10 +1,17 @@
 @CHCP 65001
+ECHO off
 
 set versaoAInstalar=1.0.2.3
 
-copy "%cd%\appsettings.json" "%cd%\repositorio\%versaoAInstalar%\Binarios\appsettings.json"
+IF /I "%1" == "install" ( 
 
-sc create SkyInfo.Servico.Agente.%versaoAInstalar% binPath="%cd%\repositorio\%versaoAInstalar%\Binarios\SkyInfo.Agente.Servico.Agente.exe" start=delayed-auto
-sc description SkyInfo.Servico.Agente.%versaoAInstalar% "Agente de sincronização dos sistemas legado com novo projeto Sky Sistemas."
-sc failure SkyInfo.Servico.Agente.%versaoAInstalar% reset=86400 actions=restart/180000/restart/180000/restart/180000
-sc start SkyInfo.Servico.Agente.%versaoAInstalar%
+    copy "%cd%\appsettings.json" "%cd%\repositorio\%versaoAInstalar%\Binarios\appsettings.json"
+
+    sc create SkyInfo.Servico.Agente.%versaoAInstalar% binPath="%cd%\repositorio\%versaoAInstalar%\Binarios\SkyInfo.Agente.Servico.Agente.exe" start=delayed-auto
+    sc description SkyInfo.Servico.Agente.%versaoAInstalar% "Agente de sincronização dos sistemas legado com novo projeto Sky Sistemas."
+    sc failure SkyInfo.Servico.Agente.%versaoAInstalar% reset=86400 actions=restart/180000/restart/180000/restart/180000
+    sc start SkyInfo.Servico.Agente.%versaoAInstalar%
+)ELSE IF /I "%1" == "uninstall" (
+    sc stop SkyInfo.Servico.Agente.%versaoAInstalar%
+    sc delete SkyInfo.Servico.Agente.%versaoAInstalar%
+)
