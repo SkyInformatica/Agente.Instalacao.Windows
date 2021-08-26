bad7469e1747431d7db30c29a6481abcf0c449ef
brunodeunersky <bruno_deuner@hotmail.com>
2021-08-26 12:34:22 -0300

    Atualizado scripts para instalar e atualizar ultima versão

--- InstalarUltimaVersao.bat
+++ InstalarUltimaVersao.bat
@@ -1,17 +1,21 @@
 @CHCP 65001
 ECHO off
 
-set versaoAInstalar=1.0.2.3
+set versaoAInstalar=1.0.2.4
 
 IF /I "%1" == "install" ( 
 
-    copy "%cd%\appsettings.json" "%cd%\repositorio\%versaoAInstalar%\Binarios\appsettings.json"
+    set caminhoDosBinariosDaVersaoAInstalar = %~dp0%versaoAInstalar%\Binarios
 
-    sc create SkyInfo.Servico.Agente.%versaoAInstalar% binPath="%cd%\repositorio\%versaoAInstalar%\Binarios\SkyInfo.Agente.Servico.Agente.exe" start=delayed-auto
+    copy "%~dp0appsettings.json" "%caminhoDosBinariosDaVersaoAInstalar%\appsettings.json"
+
+    sc create SkyInfo.Servico.Agente.%versaoAInstalar% binPath="%caminhoDosBinariosDaVersaoAInstalar%\SkyInfo.Agente.Servico.Agente.exe" start=delayed-auto
     sc description SkyInfo.Servico.Agente.%versaoAInstalar% "Agente de sincronização dos sistemas legado com novo projeto Sky Sistemas."
     sc failure SkyInfo.Servico.Agente.%versaoAInstalar% reset=86400 actions=restart/180000/restart/180000/restart/180000
     sc start SkyInfo.Servico.Agente.%versaoAInstalar%
-)ELSE IF /I "%1" == "uninstall" (
+)
+
+IF /I "%1" == "uninstall" (
     sc stop SkyInfo.Servico.Agente.%versaoAInstalar%
     sc delete SkyInfo.Servico.Agente.%versaoAInstalar%
 )
