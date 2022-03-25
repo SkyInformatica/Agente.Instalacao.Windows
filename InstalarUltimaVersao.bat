33c3479da3e3eb90fa0f3d2ae7fd88e9eded83c7
Alex Júnior Vieira <alex.vieira@skyinformatica.com.br>
2022-03-25 09:40:33 -0300

    Ajuste opções de comando

--- InstalarUltimaVersao.bat
+++ InstalarUltimaVersao.bat
@@ -7,9 +7,9 @@ IF /I "%1" == "install" (
 
     copy "%~dp0\appsettings.json" "%~dp0%versaoAInstalar%\Binarios\appsettings.json"
 
-    sc create SkyInfo.Servico.Agente.%versaoAInstalar% binPath="%~dp0%versaoAInstalar%\Binarios\SkyInfo.Agente.Servico.Agente.exe" start=delayed-auto
+    sc create SkyInfo.Servico.Agente.%versaoAInstalar% binPath= "%~dp0%versaoAInstalar%\Binarios\SkyInfo.Agente.Servico.Agente.exe" start= delayed-auto
     sc description SkyInfo.Servico.Agente.%versaoAInstalar% "Agente de sincronização dos sistemas legado com novo projeto Sky Sistemas."
-    sc failure SkyInfo.Servico.Agente.%versaoAInstalar% reset=86400 actions=restart/180000/restart/180000/restart/180000
+    sc failure SkyInfo.Servico.Agente.%versaoAInstalar% reset= 86400 actions= restart/180000/restart/180000/restart/180000
     sc start SkyInfo.Servico.Agente.%versaoAInstalar%
 )
 
